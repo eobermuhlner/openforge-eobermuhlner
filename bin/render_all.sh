6df55474219efbf93d29f8ce6ef25cbfb3696dc3
Eric Obermühlner <eobermuhlner@gmail.com>
2020-04-18 12:51:20 +0200

    add render_all.sh and re-render all images

---
+++ bin/render_all.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+imageDir="../images"
+
+for f in `find . -name *.stl`
+do
+  echo "Rendering $f"
+  sourceDir=`dirname $f`
+  sourceFile=`basename $f`
+
+  pushd "$sourceDir" >/dev/null
+
+  echo "import(\"$sourceFile\", convexity=3);" >"$sourceFile.scad"
+  openscad -q -o "$sourceFile.png" --imgsize 512,512 --colorscheme Tomorrow "$sourceFile.scad"
+  magick mogrify -geometry 256x256 "$sourceFile.png"
+  rm "$sourceFile.scad"
+
+  if [ -d  "$imageDir" ]
+  then
+    mv "$sourceFile.png" "$imageDir"
+  fi
+
+  popd >/dev/null
+done
+

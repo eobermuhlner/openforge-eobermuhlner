e53ab3a81eee1f2a95d489c0d85d26ed68c16d5f
Eric Obermühlner <eobermuhlner@gmail.com>
2020-04-22 21:32:54 +0200

    rework circle_cage_rough_stone

--- bin/resize_all.sh
+++ bin/resize_all.sh
@@ -43,7 +43,7 @@ resize_2x2_to_1x1_half() {
   resize_stl 0.5 0.5 0.5 $1.2x2.stl $1.1x1.stl
 }
 
-resize() {
+resize_from_2x2() {
   resize_2x2_to_1x1 $1
   resize_2x2_to_3x3 $1
   resize_2x2_to_4x4 $1
@@ -51,20 +51,16 @@ resize() {
 
 pushd circle-insets/stl
 
-resize_2x2_to_1x1_half circle_inset.circle_cage_rough_stone.inch
-resize_2x2_to_3x3 circle_inset.circle_cage_rough_stone.inch
-resize_2x2_to_4x4 circle_inset.circle_cage_rough_stone.inch
-
-resize circle_inset.circle_magic1_rough_stone.inch
-resize circle_inset.circle_magic2_rough_stone.inch
-resize circle_inset.circle_magic1_smooth.inch
-resize circle_inset.circle_magic2_rough_stone.inch
-resize circle_inset.circle_magic2_smooth.inch
-resize circle_inset.circle_mosaic_smooth_stone.inch
-resize circle_inset.circle_pentagram_rough_stone.inch
-resize circle_inset.circle_pentagram_smooth.inch
-resize circle_inset.circle_pentagram_smooth_stone.inch
-resize circle_inset.circle_rough_stone.inch
-resize circle_inset.circle_smooth.inch
+resize_from_2x2 circle_inset.circle_magic1_rough_stone.inch
+resize_from_2x2 circle_inset.circle_magic2_rough_stone.inch
+resize_from_2x2 circle_inset.circle_magic1_smooth.inch
+resize_from_2x2 circle_inset.circle_magic2_rough_stone.inch
+resize_from_2x2 circle_inset.circle_magic2_smooth.inch
+resize_from_2x2 circle_inset.circle_mosaic_smooth_stone.inch
+resize_from_2x2 circle_inset.circle_pentagram_rough_stone.inch
+resize_from_2x2 circle_inset.circle_pentagram_smooth.inch
+resize_from_2x2 circle_inset.circle_pentagram_smooth_stone.inch
+resize_from_2x2 circle_inset.circle_rough_stone.inch
+resize_from_2x2 circle_inset.circle_smooth.inch
 
 popd
